8f4096a3916d9df873d08f12f2fe28e56187a47c
DeadSunrise <7worldwot7@gmail.com>
2025-11-28 00:12:53 +0300

    Фз15 выполнено #1

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.model.BookingStatus;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
